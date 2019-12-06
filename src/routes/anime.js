5cbaf51cdbf75ff4e37642059983410009189311
camreyaro <camreyaro@alum.us.es>
2019-12-06 01:02:14 +0100

    created GET methods for animes from API

--- src/routes/anime.js
+++ src/routes/anime.js
@@ -5,8 +5,31 @@ const router = express.Router();
 // Gets anime list
 // GET localhost:3000/customer
 router.get('/animes', (req, res) => {
-  const response = AnimeService.getAnimes();
-  res.send(response);
+    page = req.query.page ? req.query.page : 1
+    
+    var filters = {
+        "status" : req.query.status ? req.query.status : "",
+        "genres" : req.query.genres ? req.query.genres : "",
+        "text" : req.query.text ? req.query.text : "",
+    }
+
+    AnimeService.getAnimes(page, filters).then(function (response) {
+        res.send(response.data);
+    }, function (err) {
+        console.log(err)
+    })
+
+});
+
+router.get('/animes/:id', (req, res) => {
+    anime_id = req.params.id
+    
+    AnimeService.getAnimeById(anime_id).then(function (response) {
+        res.send(response.data);
+    }, function (err) {
+        console.log(err)
+    })
+
 });
 
 module.exports = router;
--- src/services/animeService.js
+++ src/services/animeService.js
@@ -1,10 +1,47 @@
 'use strict';
 
-const API_PATH = process.env.API_PATH;
+var request = require("request");
+var utils = require("../utils");
 class AnimeService {
-  static getAnimes() {
-    return process.env.API_PATH;
-  }
+    static getAnimes(page, filters) {
+        const API_PATH = process.env.API_PATH;
+        var options = {
+            url: utils.createUrl(page, filters),
+            headers: {
+                'Accept': 'application/vnd.api+json',
+                'Content-Type': 'application/vnd.api+json'
+            }
+        };
+        return new Promise(function (resolve, reject) {
+            request.get(options, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(response.body));
+                }
+            })
+        });
+    }
+
+    static getAnimeById(id) {
+        const API_PATH = process.env.API_PATH;
+        var options = {
+            url: `${API_PATH}/anime?filter[id]=${id}`,
+            headers: {
+                'Accept': 'application/vnd.api+json',
+                'Content-Type': 'application/vnd.api+json'
+            }
+        };
+        return new Promise(function (resolve, reject) {
+            request.get(options, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(response.body));
+                }
+            })
+        });
+    }
 }
 
 module.exports = AnimeService;
