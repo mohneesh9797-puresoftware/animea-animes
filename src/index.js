6c3f44dab89107cf538d208e3439c303dbe64e93
camreyaro <camreyaro@alum.us.es>
2019-12-18 20:16:00 +0100

    added swagger to define api

--- src/index.js
+++ src/index.js
@@ -6,7 +6,37 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const database = require('../db');
 const dotenv = require('dotenv');
+const expressSwagger = require('express-swagger-generator')(app);
 
+// Swagger configuration
+let options = {
+  swaggerDefinition: {
+      info: {
+          description: 'This is a sample server',
+          title: 'Swagger',
+          version: '1.0.0',
+      },
+      host: 'localhost:3000',
+      basePath: '/v1',
+      produces: [
+          "application/json",
+          "application/xml"
+      ],
+      schemes: ['http', 'https'],
+      securityDefinitions: {
+          JWT: {
+              type: 'apiKey',
+              in: 'header',
+              name: 'Authorization',
+              description: "",
+          }
+      }
+  },
+  basedir: __dirname, //app absolute path
+  files: ['./routes/*.js'] //Path to the API handle folder
+};
+expressSwagger(options)
+// Database connection
 database.connect();
 
 app.use(bodyParser.json());
@@ -37,6 +67,6 @@ app.use((err, req, res, next) => {
   res.sendFile(path.join(__dirname, '../public/500.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.info(`Server has started on port ${PORT}`));
--- src/routes/anime.js
+++ src/routes/anime.js
@@ -2,7 +2,23 @@ const AnimeService = require('../services/animeService.js');
 const express = require('express');
 const router = express.Router();
 
-// Gets anime list
+/**
+ * @typedef Anime
+ * @property {integer} anime_id.required - Identifier of the anime
+ * @property {integer} user_id.required - Identifier of the user
+ * @property {enum} status.required - Status of the anime in the user's list - eg: pending, watching, finished
+ * @property {integer} rating - Rating given from the user for the anime
+ */
+
+/**
+ * @route GET /animes
+ * @group Anime - Operations about Anime
+ * @param {string} status.query - status of the anime for the user
+ * @param {string} status.genres - genres of the anime
+ * @param {string} status.text - text contained in the anime title or description
+ * @returns {object} 200 - An array with the searched animes
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/animes', (req, res) => {
   page = req.query.page ? req.query.page : 1;
 
@@ -19,6 +35,15 @@ router.get('/animes', (req, res) => {
   });
 });
 
+
+
+/**
+ * @route GET /animes/:id
+ * @group Anime - Operations about Anime
+ * @param {string} id.query.required - identifier of the anime
+ * @returns {object} 200 - JSON object with information about the searched anime
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/animes/:id', (req, res) => {
   animeId = req.params.id;
 
@@ -29,16 +54,32 @@ router.get('/animes/:id', (req, res) => {
   });
 });
 
+/**
+ * @route GET /user/:id/animes
+ * @group Anime - Operations about Anime
+ * @param {string} id.query.required - identifier of the user
+ * @returns {object} 200 - An array with the animes that the user has in his list
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/user/:id/animes', (req, res) => {
   userId = req.params.id;
 
   AnimeService.getUserAnimesById(userId).then(function(response) {
+    console.log(response)
     res.json(response);
   }, function(err) {
     console.log(err);
   });
 });
 
+
+/**
+ * @route DELETE /user/animes/:id
+ * @group Anime - Operations about Anime
+ * @param {string} id.query.required - identifier of the anime
+ * @returns {object} 200 - Anime was properly deleted from the user's list
+ * @returns {Error}  default - Unexpected error
+ */
 router.delete('/user/animes/:id', (req, res) => {
   animeId = req.params.id;
   //userId = req.params.id;
@@ -50,6 +91,13 @@ router.delete('/user/animes/:id', (req, res) => {
   });
 });
 
+/**
+ * @route POST /user/animes/:id
+ * @group Anime - Operations about Anime
+ * @param {string} anime.query.required - anime to save in the user'list
+ * @returns {object} 200 - Anime was properly added to the user's list
+ * @returns {Error}  default - Unexpected error
+ */
 router.post('/user/animes', (req, res) => {
   var anime = req.body;
 
@@ -60,11 +108,16 @@ router.post('/user/animes', (req, res) => {
   });
 });
 
+/**
+ * @route PUT /user/animes/:id
+ * @group Anime - Operations about Anime
+ * @param {string} anime.query.required - anime to update in the user's list
+ * @returns {object} 200 - Anime was properly deleted from the user's list
+ * @returns {Error}  default - Unexpected error
+ */
 router.put('/user/animes', (req, res) => {
   var anime = req.body;
-
   console.log(anime);
-
   AnimeService.updateUserAnimeById(anime).then(function(response){
     res.sendStatus(200);
   }, function(err){
