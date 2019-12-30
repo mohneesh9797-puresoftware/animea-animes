98a3e55ac4621709ea01106de6e2374a8b49f725
camreyaro <camreyaro@alum.us.es>
2019-12-30 16:02:20 +0100

    added mocks for Kitsu API requests

--- test/animeService.spec.js
+++ test/animeService.spec.js
@@ -2,9 +2,21 @@ const chai = require("chai");
 const request = require('supertest');
 const mongoose = require('../db.js');
 const app = require('../src/server');
+const nock = require('nock');
+const kitsuResponses = require('./kitsuResponses');
 
 const assert = chai.assert;
 const expect = chai.expect;
+
+beforeEach(() => {
+    nock('https://kitsu.io')
+        .get('/api/edge/anime?sort=popularityRank&page[limit]=10&page[offset]=0')
+        .reply(200, kitsuResponses.getAllAnimes)
+        .get('/api/edge/anime?sort=popularityRank&page[limit]=10&page[offset]=0&filter[genres]=school')
+        .reply(200, kitsuResponses.getAnimesByGenre)
+        .get('/api/edge/anime?filter[id]=207')
+        .reply(200, kitsuResponses.getAnimeById)
+});
 
 describe("Get all animes", () => {
     it('Should return a list of 10 animes', (done) => {
@@ -38,6 +50,18 @@ describe("Get anime by genre", () => {
                 expect(response.statusCode).to.be.equal(200)
                 expect(response.body[0].attributes.titles.en).to.be.equal("My Hero Academia")
                 expect(response.body[9].attributes.titles.en).to.be.equal("Food Wars! Shokugeki no Soma")
+            })
+            .expect(200, done);
+    });
+});
+
+describe("Get anime by wrong filter", () => {
+    it('Should return a list of 10 animes unfiltered', (done) => {
+        request(app)
+            .get('/animes?wrongFilter=wrongFilterValue')
+            .expect(response => {
+                expect(response.statusCode).to.be.equal(200)
+                expect(response.body).to.have.length(10)
             })
             .expect(200, done);
     });
