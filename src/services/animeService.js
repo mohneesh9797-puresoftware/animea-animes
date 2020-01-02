4db0562c91276a9b376b14a4ea16940b8db666d2
camreyaro <camreyaro@alum.us.es>
2020-01-02 18:25:09 +0100

    added cache for Kitsu API

--- src/services/animeService.js
+++ src/services/animeService.js
@@ -3,6 +3,7 @@
 const request = require('request');
 const utils = require('../utils');
 const AnimeModel = require('../models/anime.model');
+const cache = require('memory-cache');
 
 class AnimeService {
   static getAnimes(page, filters) {
@@ -13,14 +14,23 @@ class AnimeService {
         'Content-Type': 'application/vnd.api+json',
       },
     };
-    return new Promise(function(resolve, reject) {
-      request.get(options, (err, response, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(response.body));
-        }
-      });
+
+    var cachedBody = cache.get(options.url);
+    return new Promise(function (resolve, reject) {
+      if (!cachedBody) {
+        request.get(options, (err, response, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            var data = JSON.parse(response.body);
+            cache.put(options.url, data, 86400000) // the cache will be stored 24h
+            resolve(data);
+          }
+        });
+      } else {
+        console.log("Using cache...")
+        resolve(cache.get(options.url));
+      }
     });
   }
 
@@ -33,69 +43,77 @@ class AnimeService {
         'Content-Type': 'application/vnd.api+json',
       },
     };
-    return new Promise(function(resolve, reject) {
-      request.get(options, (err, response, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(response.body));
-        }
-      });
+    var cachedBody = cache.get(options.url);
+    return new Promise(function (resolve, reject) {
+      if (!cachedBody){
+        request.get(options, (err, response, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            var data = JSON.parse(response.body);
+            cache.put(options.url, data, 86400000) // the cache will be stored 24h
+            resolve(data);
+          }
+        });
+      } else {
+        console.log("Using cache...")
+        resolve(cache.get(options.url));
+      }
     });
   }
 
   static getUserAnimesById(userId) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       AnimeModel.find({
         user_id: userId,
       })
-          .then((doc) => {
-            const userAnimes = [];
-            for (let i = 0; i < doc.length; i++) {
-              const userAnime = doc[i];
-              userAnimes.push(AnimeService.getAnimeById(userAnime.anime_id));
-            }
-            Promise.all(userAnimes).then((response) => {
-              resolve(response.map((x, index) => {
-                x = x.data[0];
-                x.userData = doc[index];
-                return x;
-              }));
-            });
-          })
-          .catch((err) => {
-            reject(err);
+        .then((doc) => {
+          const userAnimes = [];
+          for (let i = 0; i < doc.length; i++) {
+            const userAnime = doc[i];
+            userAnimes.push(AnimeService.getAnimeById(userAnime.anime_id));
+          }
+          Promise.all(userAnimes).then((response) => {
+            resolve(response.map((x, index) => {
+              x = x.data[0];
+              x.userData = doc[index];
+              return x;
+            }));
           });
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
   }
 
   static deleteUserAnimeById(animeId, userId) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       AnimeModel.remove({
         anime_id: animeId,
-      }, function(err, docs) {
+      }, function (err, docs) {
         resolve();
       });
     });
   }
 
   static postUserNewAnime(anime) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       AnimeModel.create({
         'anime_id': anime.anime_id,
         'status': anime.status,
         'rating': anime.rating,
-      }, anime, function() {
+      }, anime, function () {
         resolve();
       });
     });
   }
 
   static updateUserAnimeById(anime) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       AnimeModel.update({
         'anime_id': anime.anime_id,
-      }, anime, function() {
+      }, anime, function () {
         resolve();
       });
     });
