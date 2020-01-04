f3bc4754b1d4babe3b2581307b2a8397f3a72249
camreyaro <camreyaro@alum.us.es>
2020-01-04 15:49:33 +0100

    test integration

--- db.js
+++ db.js
@@ -9,6 +9,10 @@ const PASSWORD = process.env.PASSWORD;
 const OPTIONS = process.env.OPTIONS;
 
 function connect() {
+  console.log(USER)
+  console.log(PASSWORD)
+  console.log(DATABASE)
+  console.log(SERVER)
   mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}/${DATABASE}?${OPTIONS}`);
 }
 
