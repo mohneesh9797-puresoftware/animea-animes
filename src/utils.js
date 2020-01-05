ff24d27411e41cab9f642635bc887b082e604419
camreyaro <camreyaro@alum.us.es>
2020-01-05 17:26:09 +0100

    url creator changed

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,12 @@
 module.exports = {
   createUrl: function(page, filters) {
     const API_PATH = process.env.API_PATH;
-    let apiUrl = `${API_PATH}/anime?sort=popularityRank&page[limit]=10&page[offset]=${page}`;
+    let apiUrl;
     if (filters.text) {
+      apiUrl = `${API_PATH}/anime?page[limit]=10&page[offset]=${page}`;
       apiUrl += `&filter[text]=${filters.text}`;
+    } else {
+      apiUrl = `${API_PATH}/anime?sort=popularityRank&page[limit]=10&page[offset]=${page}`;
     }
 
     if (filters.status) {
