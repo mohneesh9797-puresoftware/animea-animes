685660bcef477048c587b3d8ec4a149473ad250d
camreyaro <camreyaro@alum.us.es>
2020-01-07 11:33:23 +0100

    user env changed

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const server = require("./server.js")
 database.connect();
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = (process.env.PORT || 3000);
 server.listen(PORT, () => console.info(`Server has started on port ${PORT}`));
